8e32f7d5a9c0680a9a30b60216cc350e1000818e
Dhuha Nawab <mac@MacBook-Air-alkhas-b-mac.local>
2020-12-11 21:14:40 +0300

    functions

--- DHUHA BIKESHARE.py
+++ DHUHA BIKESHARE.py
@@ -8,7 +8,7 @@ import time
 CITY_DATA = {'chicago': 'chicago.csv',
              'new york city': 'new_york_city.csv',
              'washington': 'washington.csv'}
-
+#Functions#
     # Function 1 # get_filters () #
 """
     #Returns:
