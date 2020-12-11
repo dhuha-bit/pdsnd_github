b49fb8b7ce390a6bc6adac53ce7d6f90d060b97e
Dhuha Nawab <mac@MacBook-Air-alkhas-b-mac.local>
2020-12-11 21:09:30 +0300

    importing data

--- DHUHA BIKESHARE.py
+++ DHUHA BIKESHARE.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import time
 
+#importing data 
 CITY_DATA = {'chicago': 'chicago.csv',
              'new york city': 'new_york_city.csv',
              'washington': 'washington.csv'}
